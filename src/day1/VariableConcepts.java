2b2b718f9dbf2507b2821072fc8e4f5135577146
Ashish Mishra <ashish.mishra.sse@gmail.com>
2026-03-12 06:09:16 +0530

    day 1: Variable Concepts

---
+++ src/day1/VariableConcepts.java
@@ -0,0 +1,22 @@
+package day1;
+
+public class VariableConcepts {
+
+    public static void main(String[] args) {
+
+        //What are Variables? Variables are like boxes/jara/containers which is needed to store something
+        //Syntax:
+        //1. dataType variableName=value;
+        //I want to add two numbers [12,8]
+
+        int x = 12;
+        int y = 8;
+
+        //data Types: byte, short, int, long, float, double, char, boolean
+
+        // I want to add two decimal numbers[12.34, 6.78]
+
+        double a = 12.34;
+        double b = 6.78;
+    }
+}
